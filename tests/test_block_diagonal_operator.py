a37f062dc88cf9ba54ebbb469869493cb6cad8a2
giuspugl <giuspugl@sissa.it>
2016-02-25 09:51:50 +0100

    fixed bugs in test_block_diagonal_operator.py

--- tests/test_block_diagonal_operator.py
+++ tests/test_block_diagonal_operator.py
@@ -10,12 +10,13 @@ def test_block_diagonal_operator():
     test the :class:`BlockDiagonalLO`.
     """
     runcase={'I':1,'QU':2,'IQU':3}
-    a=[64,128,256,512]
+    a=[64,128,256]
     nb=1
-    for nt in [2**12,2**16]:
+    for nt in [2**14,2**15]:
         for pol in runcase.values():
             blocksize=nt/nb
             for i in a:
+                print pol*i,nt
                 npix=int(i)
                 x=np.ones(pol*npix)
                 d,pairs,phi,t,diag=system_setup(nt,npix,nb)
@@ -25,10 +26,12 @@ def test_block_diagonal_operator():
                 #invMbd and P.T*P  operate in the same on to a pixel vector
                 y=invMbd*x
                 y2=P.T*P*x
-                assert np.allclose(y,y2)
+                #show_matrix_form(invMbd*Mbd)
+                assert np.allclose(y[P.mask],y2[P.mask])
 
                 # invMbd*Mbd = Identity
+
                 v=Mbd*invMbd*x
-                assert np.allclose(v,x)
+                assert np.allclose(v[P.mask],x[P.mask])
 
 test_block_diagonal_operator()
